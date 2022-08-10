b1f77bbc574ddb31f2def2802b412ebf645478c8
cuihao <1584808019@qq.com>
2022-08-10 13:13:34 +0800

    all

---
+++ ReduxDemo/demo01/src/store/reducer.js
@@ -0,0 +1,12 @@
+const defaultState = {
+    inputValue: 'Write Someing',
+    list: [
+        '早8点开晨会, 分配今天的代码任务',
+        '早9点和项目经理开需求沟通会',
+        '早9点和项目经理开需求沟通会'
+    ]
+}
+
+export default (state = defaultState, action) => {
+    return state
+}
